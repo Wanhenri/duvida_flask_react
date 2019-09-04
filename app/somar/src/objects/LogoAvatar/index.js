0c04648030581f72707fd55c92d731e7e8c36e2a
Wanderson Henrique <slipkorn.jk@gmail.com>
2019-09-04 16:07:08 -0300

    Add files via upload

---
+++ app/somar/src/objects/LogoAvatar/index.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+import avatar from "../../img/logo-institucional.png";
+
+const LogoAvatar = styled.img.attrs({
+    src: avatar
+})`
+    width:10%;
+    padding-left:15px;
+`;
+
+export default LogoAvatar;
---
+++ app/somar/src/objects/SubtitleSomar/index.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const SubtitleSomar = styled.h2 `
+    color: var(--color-zero);
+    font-size: var(--size-medium);
+    margin-bottom: var(--spacing-big);
+    text-align:center;
+    width: 500px; 
+    position: absolute;
+    top:90px;
+    left:450px;
+`;
+
+export default SubtitleSomar;
