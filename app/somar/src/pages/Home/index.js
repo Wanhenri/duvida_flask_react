fa58731ffcf8316b985ad402feab6d0e1bd6e4fb
Wanderson Henrique <slipkorn.jk@gmail.com>
2019-09-04 16:05:59 -0300

    Add files via upload

---
+++ app/somar/src/pages/Home/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { Wrapper, Section } from './styles';
+
+import HeaderSomar from "../../components/HeaderSomar";
+
+// import FormLead from '../../components/FormLead';
+
+
+ const Home = () => (
+    <Wrapper>
+
+        <HeaderSomar />
+        
+        
+        <Section>
+            
+        </Section> 
+   
+        
+    </Wrapper>
+ );
+
+export default Home;
---
+++ app/somar/src/pages/Home/styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Section = styled.section`
+    display:flex;
+    flex-direction: column;
+    align-items:center;
+    width:60%;
+    min-width: 545px; 
+ 
+   
+`;
+
+export const Wrapper = styled.main `
+    background-color: var(--color-first);
+    padding-left:80px;
+    height:100vh;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+`;
