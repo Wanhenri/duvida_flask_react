9e15eee30967ad5e3258a94918db7ea5497ab93a
Matheus dos Reis de Jesus <matheusdrdj@gmail.com>
2019-10-28 20:21:43 -0300

    [feat] (INPE): Convertendo XML para JSON

--- api/routes/inpe.py
+++ api/routes/inpe.py
@@ -12,16 +12,20 @@ class prevInpe:
         response_forecast = requests.get(
             'http://servicos.cptec.inpe.br/XML/capitais/condicoesAtuais.xml')
         #prevCapitais = ElementTree.fromstring(response_forecast.content)
-        return response_forecast.content
+        tree = ElementTree.fromstring(response_forecast.content)
+        content = []
+        for tag in tree.findall("metar"):
+            metar = {}
+            metar["codigo"] = tag.find("codigo").text
+            metar["atualizacao"] = tag.find("atualizacao").text
+            metar["pressao"] = tag.find("pressao").text
+            metar["temperatura"] = tag.find("temperatura").text
+            metar["tempo_desc"] = tag.find("tempo_desc").text
+            content.append(metar)
+        return content
 
 
 class Inpe(Resource):
     def get(self):
-        return {
-            'status': 200,
-            'response': "Funciona"
-        }
-
-    def post(self):
         inpe_reports = prevInpe().get_prevInpe()
-        return Response(inpe_reports, mimetype="application/xml")
+        return inpe_reports
