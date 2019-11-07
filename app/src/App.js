c0b5546ee1653de90f4847b95dbd622c5831bc4d
wanhenri <slipkorn.jk@gmail.com>
2019-11-07 18:11:49 -0300

    testando componentes no app

--- app/src/App.js
+++ app/src/App.js
@@ -8,6 +8,8 @@ import Base from "./styles/base";
 import Home from "./pages/Home";
 import Size from "./styles/settings/size";
 
+import ReactEcharts from "echarts-for-react";
+
 const App = () => (
   <Fragment>
     <Colors />
@@ -15,7 +17,99 @@ const App = () => (
     <Spacing />
     <Reset />
     <Base />
-    <Home />
+    <Home />    
+  
+  <ReactEcharts
+    option={{
+      xAxis: {
+        type: "category",
+        data: ["Mon", "True", "Wed", "Thu", "Fri", "Sat", "Sun"]
+      },
+      yAxis: {
+        type: "value"
+      },
+      series: [{
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        type: "line"
+      }]
+    }}
+  />
+
+<ReactEcharts    
+    option = {{
+      backgroundColor: '#2c343c',
+
+    title: {
+        text: 'Customized Pie',
+        left: 'center',
+        top: 20,
+        textStyle: {
+            color: '#ccc'
+        }
+    },
+
+    tooltip : {
+        trigger: 'item',
+        formatter: "{a} <br/>{b} : {c} ({d}%)"
+    },
+
+    visualMap: {
+        show: false,
+        min: 80,
+        max: 600,
+        inRange: {
+            colorLightness: [0, 1]
+        }
+    },
+    series : [
+        {
+            name:'访问来源',
+            type:'pie',
+            radius : '55%',
+            center: ['50%', '50%'],
+            data:[
+                {value:335, name:'直接访问'},
+                {value:310, name:'邮件营销'},
+                {value:274, name:'联盟广告'},
+                {value:235, name:'视频广告'},
+                {value:400, name:'搜索引擎'}
+            ].sort(function (a, b) { return a.value - b.value; }),
+            roseType: 'radius',
+            label: {
+                normal: {
+                    textStyle: {
+                        color: 'rgba(255, 255, 255, 0.3)'
+                    }
+                }
+            },
+            labelLine: {
+                normal: {
+                    lineStyle: {
+                        color: 'rgba(255, 255, 255, 0.3)'
+                    },
+                    smooth: 0.2,
+                    length: 10,
+                    length2: 20
+                }
+            },
+            itemStyle: {
+                normal: {
+                    color: '#c23531',
+                    shadowBlur: 200,
+                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                }
+            },
+
+            animationType: 'scale',
+            animationEasing: 'elasticOut',
+            animationDelay: function (idx) {
+                return Math.random() * 200;
+            }
+        }
+    ]
+}}
+  />
+
   </Fragment>
 );
 
