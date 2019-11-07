298bf1246d7fff894ce851273ebd9931604dba18
wanhenri <slipkorn.jk@gmail.com>
2019-11-07 18:16:13 -0300

    atualizado

--- app/src/pages/Home/index.js
+++ app/src/pages/Home/index.js
@@ -4,8 +4,9 @@ import { Wrapper, Section } from "./styles";
 
 import HeaderSomar from "../../components/HeaderSomar";
 import FetSomar from "./../../components/FetSomar";
+import EchartGraph from "../../components/EchartLine"
 
-import HeaderDays from "./../../components/HeaderDay"
+// import HeaderDays from "./../../components/HeaderDay"
 
 // import FormLead from '../../components/FormLead';
 
@@ -14,6 +15,7 @@ const Home = () => (
     <HeaderSomar />
     <Section>
       <FetSomar />
+      <EchartGraph />
     </Section>
   </Wrapper>
 );
