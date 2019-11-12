2623bdbabc784b4cd6a4a95cb5901b3c2070e20a
wanhenri <slipkorn.jk@gmail.com>
2019-11-11 23:39:11 -0300

    criado componente TimeSummary

---
+++ app/src/objects/TimeSummary/index.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+
+const TimeSummary = styled.header`
+    background: linear-gradient(to bottom, #005dea, #0069ee, #0075f0, #0080f2, #008af3, #0098f8, #00a6fc, #00b3fe, #00c7ff, #00daff, #1bebf9, #5ffbf1);
+    box-sizing:border-box;
+    
+    
+    width: 100vw;    
+    height: 120px;
+    position: absolute;    
+    top:100%;
+    right:70%;
+    
+    
+    align-items: left;
+    justify-content:left;
+    
+`;
+
+export default TimeSummary;
