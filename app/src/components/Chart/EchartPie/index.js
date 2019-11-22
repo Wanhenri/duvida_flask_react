568af83792c284e261434d12e77284769216ea83
Matheus dos Reis de Jesus <matheusdrdj@gmail.com>
2019-11-21 21:55:24 -0300

    Configurando EChartPie

--- app/src/components/Chart/EchartPie/index.js
+++ app/src/components/Chart/EchartPie/index.js
@@ -1,132 +1,130 @@
-import React, {Component} from 'react';
-import { Wrapper, Section} from './styles';
+import React, { Component } from "react";
+import { Wrapper, Section } from "./styles";
 import ReactEcharts from "echarts-for-react";
 
 class EchartGraphPie extends Component {
-    constructor(props, context) {
-        super(props, context);
-        this.state = {
-            graphOption: {
-                backgroundColor: '#2c343c',
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      graphOption: {
+        title: {
+          text: "Temperatura das Capitais",
+          left: "center",
+          top: 20,
+          textStyle: {
+            color: "#ccc"
+          }
+        },
+        tooltip: {
+          trigger: "item",
+          formatter: "{a} <br/>{b} : {c} ({d}%)"
+        },
+        visualMap: {
+          show: false,
+          min: 0,
+          max: 50,
+          inRange: {
+            colorLightness: [0, 1]
+          }
+        },
+        series: [
+          Object.assign(this.defaultSeries, {
+            name: "Capitais",
+            type: "pie",
+            data: [
+              { value: 335, name: "直接访问" },
+              { value: 310, name: "邮件营销" },
+              { value: 274, name: "联盟广告" },
+              { value: 235, name: "视频广告" },
+              { value: 400, name: "搜索引擎" }
+            ].sort(function(a, b) {
+              return a.value - b.value;
+            })
+          })
+        ]
+      }
+    };
+    console.log(this.state);
+  }
 
-                title: {
-                    text: 'Temperatura das Capitais',
-                    left: 'center',
-                    top: 20,
-                    textStyle: {
-                        color: '#ccc'
-                    }
-                },
-            
-                tooltip : {
-                    trigger: 'item',
-                    formatter: "{a} <br/>{b} : {c} ({d}%)"
-                },
-            
-                visualMap: {
-                    show: false,
-                    min: 80,
-                    max: 600,
-                    inRange: {
-                        colorLightness: [0, 1]
-                    }
-                },
-                series : [
-                    {
-                        name:'Capitais',
-                        type:'pie',
-                        radius : '55%',
-                        center: ['50%', '50%'],
-                        data:[
-                            {value:335, name:'直接访问'},
-                            {value:310, name:'邮件营销'},
-                            {value:274, name:'联盟广告'},
-                            {value:235, name:'视频广告'},
-                            {value:400, name:'搜索引擎'}
-                        ].sort(function (a, b) { return a.value - b.value; }),
-                        roseType: 'radius',
-                        label: {
-                            normal: {
-                                textStyle: {
-                                    color: 'rgba(255, 255, 255, 0.3)'
-                                }
-                            }
-                        },
-                        labelLine: {
-                            normal: {
-                                lineStyle: {
-                                    color: 'rgba(255, 255, 255, 0.3)'
-                                },
-                                smooth: 0.2,
-                                length: 10,
-                                length2: 20
-                            }
-                        },
-                        itemStyle: {
-                            normal: {
-                                color: '#c23531',
-                                shadowBlur: 200,
-                                shadowColor: 'rgba(0, 0, 0, 0.5)'
-                            }
-                        },
-                    
-                        animationType: 'scale',
-                        animationEasing: 'elasticOut',
-                        animationDelay: function (idx) {
-                            return Math.random() * 200;
-                        }
-                    }
-                ]
-            }
-        }
-    }
-
-    componentDidMount() {
-        this.echartsInstance = this.echartsReactRef.getEchartsInstance();
-        this.zr = this.echartsInstance.getZr();
-
-        this.zr.on('click', this.onChartClick);
-    }
-
-    componentDidUpdate(prevProps) {
-        if (this.props.series !== prevProps.series) {
-          const newOptions = Object.assign(this.state.graphOption, {
-            title: [{ text: this.props.textTile}],
-            series: this.props.series.map(s => {
-              return {
-                name: s.name,
-                data: s.data,
-                type: "pie"
-              };
-            }),
-            legend: {
-              data: this.props.series.map(s => s.name)
-            }
-          });
-          this.echartsReactRef.getEchartsInstance().setOption(newOptions);
+  defaultSeries = {
+    radius: "55%",
+    center: ["50%", "50%"],
+    type: "pie",
+    roseType: "radius",
+    label: {
+      normal: {
+        textStyle: {
+          color: "rgba(255, 255, 255, 0.3)"
         }
       }
-
-    onChartClick = (...rest) => {
-        console.log('App:onClickChart', rest);
-    };
-    
-    render() {
-        return (
-            <Wrapper>
-                <Section>
-
-                    <ReactEcharts
-                        style={{ height: "40vh", width: "70vw", margin: "auto" }}
-                        ref={(e) => {
-                            this.echartsReactRef = e;
-                        }}
-                        option={this.state.graphOption}
-                    />
-                </Section>
-            </Wrapper>
-        );
+    },
+    labelLine: {
+      normal: {
+        lineStyle: {
+          color: "rgba(255, 255, 255, 0.3)"
+        },
+        smooth: 0.2,
+        length: 10,
+        length2: 20
+      }
+    },
+    itemStyle: {
+      normal: {
+        color: "#c26531",
+        shadowBlur: 50,
+        shadowColor: "rgba(0, 0, 0, 0.5)"
+      }
+    },
+    animationType: "scale",
+    animationEasing: "elasticOut",
+    animationDelay: function(idx) {
+      return Math.random() * 200;
     }
+  };
+
+  componentDidMount() {
+    this.echartsInstance = this.echartsReactRef.getEchartsInstance();
+    this.zr = this.echartsInstance.getZr();
+
+    this.zr.on("click", this.onChartClick);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.series !== prevProps.series) {
+      const newOptions = Object.assign(this.state.graphOption, {
+        title: [{ text: this.props.textTile }],
+        series: this.props.series.map(s => {
+          return Object.assign(this.defaultSeries, s);
+        }),
+        legend: {
+          data: this.props.series.map(s => s.name)
+        }
+      });
+      this.echartsReactRef.getEchartsInstance().setOption(newOptions);
+    }
+  }
+
+  onChartClick = (...rest) => {
+    console.log("App:onClickChart", rest);
+  };
+
+  render() {
+    console.log(this.props.series);
+    return (
+      <Wrapper>
+        <Section>
+          <ReactEcharts
+            style={{ height: "40vh", width: "70vw", margin: "auto" }}
+            ref={e => {
+              this.echartsReactRef = e;
+            }}
+            option={this.state.graphOption}
+          />
+        </Section>
+      </Wrapper>
+    );
+  }
 }
 
 export default EchartGraphPie;
--- app/src/components/FetInpe/index.js
+++ app/src/components/FetInpe/index.js
@@ -4,7 +4,6 @@ import { Card, Container } from "./style";
 import EchartGraphPie from "../Chart/EchartPie";
 import EchartGraphBar from "../Chart/EchartBar";
 
-
 const FetInpe = () => {
   const [weather, setWeather] = useState([]);
   useEffect(() => {
@@ -33,24 +32,55 @@ const FetInpe = () => {
         ))}
       </Container>
       <Container>
-          <EchartGraphPie
-            series={[
-                {
-                  name: "Capitais",
-                  data: weather.map(w => w.codigo)
-                },
-                {
-                  name: "Temperatura",
-                  data: weather.map(w => w.temperatura)
+        <EchartGraphPie
+          series={[
+            {
+              name: "Temperatura",
+              radius: "55%",
+              center: ["50%", "50%"],
+              data: weather
+                .map(w => {
+                  return { name: w.codigo, value: w.temperatura };
+                })
+                .sort(function(a, b) {
+                  return a.value - b.value;
+                }),
+              label: {
+                normal: {
+                  textStyle: {
+                    color: "rgba(255, 255, 255, 0.3)"
+                  }
                 }
-              ]}
-              type={"pie"}
-              textTile={"Temperatura das Capitais"}
-              limit={5}
-
-          />
-      <EchartGraphBar />
-      </Container>      
+              },
+              labelLine: {
+                normal: {
+                  lineStyle: {
+                    color: "rgba(255, 255, 255, 0.3)"
+                  },
+                  smooth: 0.2,
+                  length: 10,
+                  length2: 20
+                }
+              },
+              itemStyle: {
+                normal: {
+                  color: "#242424",
+                  shadowBlur: 200,
+                  shadowColor: "rgba(0, 0, 0, 0.5)"
+                }
+              },
+              animationType: "scale",
+              animationEasing: "elasticOut",
+              animationDelay: function(idx) {
+                return Math.random() * 200;
+              }
+            }
+          ]}
+          textTile={"Temperatura das Capitais"}
+          limit={5}
+        />
+        <EchartGraphBar />
+      </Container>
     </Container>
   );
 };
