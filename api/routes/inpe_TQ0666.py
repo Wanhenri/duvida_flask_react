5afbcbff0cf19a1a9d750e3b2e4cd931d415d2f0
wanhenri <slipkorn.jk@gmail.com>
2020-01-30 15:53:15 -0300

    criação de novo endpoint com a nova API + acrescimo de route

---
+++ api/routes/inpe_TQ0666.py
@@ -0,0 +1,51 @@
+import requests
+import json
+from datetime import date, timedelta
+import datetime
+from flask_restful import Resource
+
+#Para obter o valor de 00
+#Necessario colocar esse parametro de entrada na chamado no component ReactJS
+hh=str(0).zfill(2) #https://stackoverflow.com/questions/39402795/how-to-pad-a-string-with-leading-zeros-in-python-3
+
+
+StartDate = date.today() #Dia inicial
+BeforeDate = StartDate - timedelta(days=1)  #Caso o dia inicial não exista, ele continua lendo o dia anterior
+
+#Parametro formatados que passarão na função get_weather_TQ0666(Start,Before)
+Start = StartDate.strftime("%Y/%m/%d/") + hh   #formatação padrao para a leitura no link + hh(sendo 00 ou 12)
+Before = BeforeDate.strftime("%Y/%m/%d/") + hh #formatação padrao para a leitura no link + hh(sendo 00 ou 12) 
+
+
+class definirLink:
+    def __init__(self, dateJson):
+      self.dateJson = dateJson
+        
+    def Link(self):
+      return "http://ftp.cptec.inpe.br/modelos/tempo/BAM/TQ0666L064/recortes/grh/json/"+ str(self.dateJson) +"/220.json"  
+
+def get_weather_TQ0666(Start, Before):
+  cclasStart = definirLink(Start)
+  cclasBefore = definirLink(Before)
+
+  url=cclasStart.Link()
+  response_forecast = requests.get(url)
+  try:
+    json_res_forecast = response_forecast.json()
+    print("Start OK")
+  except:
+    url=cclasBefore.Link()
+    response_forecast = requests.get(url)
+    json_res_forecast = response_forecast.json()
+    print("Before OK")
+    return json_res_forecast
+    
+  return json_res_forecast
+  #print(json.dumps(json_res_forecast, indent=2, sort_keys=True))
+
+class Inpe_TQ0666(Resource):
+    def get(self):
+        reports = get_weather_TQ0666(Start,Before)
+        return reports
+        #print(reports)
+        #print(json.dumps(reports, indent=2, sort_keys=True))
--- api/server.py
+++ api/server.py
@@ -6,6 +6,7 @@ from flask_cors import CORS
 from routes.somar import Somar
 from routes.teste import Teste
 from routes.inpe import Inpe
+from routes.inpe_TQ0666 import Inpe_TQ0666
 
 app = Flask(__name__)
 CORS(app)
@@ -14,6 +15,7 @@ api = Api(app)
 api.add_resource(Somar, '/somar')
 api.add_resource(Teste, '/teste')
 api.add_resource(Inpe, '/inpe')
+api.add_resource(Inpe_TQ0666, '/inpe_TQ0666')
 
 if __name__ == '__main__':
     app.run(debug=True)
