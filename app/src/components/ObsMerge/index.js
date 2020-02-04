e060d540b2b9fbcf687774c0adcc98ae7e8b5026
wanhenri <slipkorn.jk@gmail.com>
2020-02-04 11:13:20 -0300

    [refactor](APP): ObsMerge + Ftp

--- app/src/components/ObsMerge/index.js
+++ app/src/components/ObsMerge/index.js
@@ -2,17 +2,32 @@ import React, { useState } from "react";
 
 import moment from "moment";
 
-export default function ObsMerge() {
-  const [date, setDate] = useState(
-    moment()
-      .subtract(1, "days")
-      .format("YYYYMMDD")
-  );
-  return (
+//export default function ObsMerge() {
+//  const [date, setDate] = useState(
+//    moment()
+//      .subtract(1, "days")
+//      .format("YYYYMMDD")
+//  );
+//  return (
+//    <img
+//      height="720"
+//      alt="preciptation gif for yesterday"
+//      src={`http://ftp.cptec.inpe.br/etamdl/Products/OBS-MERGE/${date}/precip_${date}.gif`}
+//    ></img>
+//  );
+//}
+//
+//const dateYesterday =  moment().subtract(1, "days").format("YYYYMMDD")
+//console.log(dateYesterday)
+
+export default function ObsMerge(props) {
+  return(
     <img
-      height="720"
-      alt="preciptation gif for yesterday"
-      src={`http://ftp.cptec.inpe.br/etamdl/Products/OBS-MERGE/${date}/precip_${date}.gif`}
+      height={props.height}
+      alt={props.title}
+      src={`http://ftp.cptec.inpe.br/etamdl/Products/OBS-MERGE/${props.date}/precip_${props.date}.gif`}
     ></img>
-  );
+  )
 }
+
+
--- app/src/pages/Ftp/index.js
+++ app/src/pages/Ftp/index.js
@@ -1,11 +1,22 @@
 import React from "react";
 import { Wrapper, Section } from "./styles";
 import ObsMerge from "../../components/ObsMerge";
+import moment from "moment";
+
+const dateY1 =  moment().subtract(1, "days").format("YYYYMMDD")
+const dateY2 =  moment().subtract(2, "days").format("YYYYMMDD")
+const dateY3 =  moment().subtract(3, "days").format("YYYYMMDD")
+const dateY4 =  moment().subtract(4, "days").format("YYYYMMDD")
+const dateY5 =  moment().subtract(5, "days").format("YYYYMMDD")
 
 const  FTP = () => (
   <Wrapper>
-    <Section>
-      <ObsMerge />
+    <Section style={{ flexDirection: "row"}}>      
+      <ObsMerge height="415" title="preciptation gif for yesterday" date={dateY1} />
+      <ObsMerge height="415" title="preciptation gif for yesterday" date={dateY2} />
+      <ObsMerge height="415" title="preciptation gif for yesterday" date={dateY3} />
+      <ObsMerge height="415" title="preciptation gif for yesterday" date={dateY4} />
+      <ObsMerge height="415" title="preciptation gif for yesterday" date={dateY5} />
     </Section>
   </Wrapper> 
 );
