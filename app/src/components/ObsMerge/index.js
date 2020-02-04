570708b11386d237685017244711dad21ddbc13d
wanhenri <slipkorn.jk@gmail.com>
2020-02-04 11:19:20 -0300

    [docs](ObsMerge): retirando comentarios

--- app/src/components/ObsMerge/index.js
+++ app/src/components/ObsMerge/index.js
@@ -2,24 +2,6 @@ import React, { useState } from "react";
 
 import moment from "moment";
 
-//export default function ObsMerge() {
-//  const [date, setDate] = useState(
-//    moment()
-//      .subtract(1, "days")
-//      .format("YYYYMMDD")
-//  );
-//  return (
-//    <img
-//      height="720"
-//      alt="preciptation gif for yesterday"
-//      src={`http://ftp.cptec.inpe.br/etamdl/Products/OBS-MERGE/${date}/precip_${date}.gif`}
-//    ></img>
-//  );
-//}
-//
-//const dateYesterday =  moment().subtract(1, "days").format("YYYYMMDD")
-//console.log(dateYesterday)
-
 export default function ObsMerge(props) {
   return(
     <img
